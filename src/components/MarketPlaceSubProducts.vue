bd4d0f7f54c5d5485326e7bf0ef58403c701e909
Zignuts Parth Gupta <parthg@zignuts.com>
2024-03-07 17:24:02 +0530

    data is storing in the local storage

--- src/components/MarketPlaceSubProducts.vue
+++ src/components/MarketPlaceSubProducts.vue
@@ -1,17 +1,24 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, onBeforeUnmount } from 'vue'
 import { useRouter } from 'vue-router'
 import { items } from '../ItemProducts'
+
+const STORAGE_KEY = 'cartItems';
 
 const value = ref(items)
 const router = useRouter()
 const currentId = ref(router.currentRoute.value.params.id)
 const currentSubcategories = ref([])
-const dialogVisible = ref(false);
+const dialogVisible = ref(false)
+const cart = ref(loadCartFromLocalStorage())
 
 const openDialog = () => {
-  dialogVisible.value = true;
-};
+  dialogVisible.value = true
+}
+
+const closeDialog = () => {
+  dialogVisible.value = false
+}
 
 const filterSubcategories = (itemId) => {
   const currentItem = value.value.find((item) => item.id === itemId)
@@ -23,41 +30,87 @@ const updateSubcategories = () => {
 }
 
 const addToCart = (item) => {
+  cart.value.push(item)
+  saveCartToLocalStorage(cart.value);
   console.log(`Added ${item.title} to the cart`)
 }
 
+const removeFromCart = (index) => {
+  const removedItem = cart.value.splice(index, 1)[0]
+  saveCartToLocalStorage(cart.value);
+  console.log(`Removed ${removedItem.title} from the cart`)
+}
+
+const calculateTotalAmount = () => {
+  return cart.value.reduce((total, item) => total + item.price, 0).toFixed(2)
+}
+
 onMounted(() => {
-  updateSubcategories() 
+  updateSubcategories()
   router.afterEach(() => {
     currentId.value = router.currentRoute.value.params.id
     updateSubcategories()
   })
 })
+
+onBeforeUnmount(() => {
+  saveCartToLocalStorage(cart.value);
+})
+
+function loadCartFromLocalStorage() {
+  const storedItems = localStorage.getItem(STORAGE_KEY);
+  return storedItems ? JSON.parse(storedItems) : [];
+}
+
+function saveCartToLocalStorage(cartItems) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(cartItems));
+}
 </script>
+
 
 <template>
   <div>
     <v-dialog v-model="dialogVisible" fullscreen>
-        <v-card>
-          <v-card-title>
-            <span class="headline">Shopping Cart</span>
-            <v-btn icon @click="dialogVisible = false" class="float-end">
-              <v-icon >mdi-close</v-icon>
-            </v-btn>
-          </v-card-title>
-          <v-card-text>
-            <!-- Place your shopping cart content here -->
-            <!-- You can customize this part based on your needs -->
-            <!-- For example, display the items in the cart, their quantities, etc. -->
-            <div v-for="(item, index) in cartItems" :key="index">
-              {{ item.title }} - Quantity: {{ item.quantity }}
-            </div>
-          </v-card-text>
-        </v-card>
-      </v-dialog>
+      <v-card>
+        <v-card-title class="d-flex align-center justify-space-between">
+          <span class="headline">Shopping Cart</span>
+          <v-btn icon @click="closeDialog">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-card-title>
+        <v-divider></v-divider>
+        <v-list>
+          <v-list-item-group v-if="cart.length > 0">
+            <v-list-item v-for="(cartItem, index) in cart" :key="index">
+              <v-list-item-content>
+                <v-list-item-title>{{ cartItem.title }}</v-list-item-title>
+                <v-list-item-subtitle>{{ `$${cartItem.price.toFixed(2)}` }}</v-list-item-subtitle>
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-btn icon @click="removeFromCart(index)">
+                  <v-icon>mdi-delete</v-icon>
+                </v-btn>
+              </v-list-item-action>
+            </v-list-item>
+          </v-list-item-group>
+          <v-list-item v-else>
+            <v-list-item-content>No items in the cart</v-list-item-content>
+          </v-list-item>
+        </v-list>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn @click="closeDialog">Close</v-btn>
+          <span>Total Amount: ${{ calculateTotalAmount() }}</span>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 
-    <h1>{{ currentSubcategories[0]?.parentCategory }} <VIcon class="float-end mt-5" size="30" @click="openDialog" >mdi mdi-cart-check</VIcon></h1>
-    
+    <h1>
+      {{ currentSubcategories[0]?.parentCategory }}
+      <VIcon class="float-end mt-5" size="30" @click="openDialog">mdi mdi-cart-check</VIcon>
+    </h1>
+
     <v-row class="pt-5">
       <v-col
         v-for="(subcategory, index) in currentSubcategories"
@@ -73,7 +126,9 @@ onMounted(() => {
           <div class="custom-card-title">{{ subcategory.title }}</div>
           <div class="custom-card-text">{{ subcategory.description }}</div>
           <div class="custom-card-subtitle">{{ `$${subcategory.price.toFixed(2)}` }}</div>
-          <v-btn @click="addToCart(subcategory)" class="custom-card-button"> <VIcon>mdi mdi-cart-plus</VIcon>Add to Cart</v-btn>
+          <v-btn @click="addToCart(subcategory)" class="custom-card-button">
+            <VIcon>mdi mdi-cart-plus</VIcon>Add to Cart</v-btn
+          >
         </v-card>
       </v-col>
     </v-row>
